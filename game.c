b8c86a05722e489c008063130063815a5962db91
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-02-04 03:33:29 +0000

    Refactored the program.

--- game.c
+++ game.c
@@ -1,54 +1,67 @@
 #include <stdio.h>
 #include <string.h>
 
+// GLOVAL VARIABLES
+char secretWord[20];
+char kicks[26];
+int attempt = 0;
+
 void opening() {
 	printf("-+-----------------+-\n");
 	printf(" |  Jogo da Forca  | \n");
 	printf("-+-----------------+-\n\n");
 }
 
-void kickCapture(char kicks[26], int* attempt) {
+void kickCapture() {
 	char kick;
 	scanf(" %c", &kick);
 
-	kicks[(*attempt)] = kick;
-	(*attempt)++;
+	kicks[attempt] = kick;
+	attempt++;
+}
+
+int alreadyKicked(char word) {
+	int find = 0;
+
+	for (int j = 0; j < attempt; j++) {
+		if (kicks[j] == word) {
+			find = 1;
+			break;
+		}
+	}
+
+	return find;
+}
+
+void draw() {
+	for (int i = 0; i < strlen(secretWord); i++) {
+		int find = alreadyKicked(secretWord[i]);
+
+		if (find) {
+			printf("%c ", secretWord[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+	printf("\n");
+}
+
+void selectWord() {
+	// SPRINTF é a função para colocar uma 'string' dentro de um ARRAY
+	sprintf(secretWord, "Giordano");
 }
 
 int main() {
-	char secretWord[20];
-
-	// SPRINTF é a função para colocar uma 'string' dentro de um ARRAY
-	sprintf(secretWord, "Giordano");
-
 	int finish = 0;
 	int hanged = 0;
 
-	char kicks[26];
-	int attempt = 0;
-
+	selectWord();
 	opening();
 
 	do {
-		for (int i = 0; i < strlen(secretWord); i++) {
-			int find = 0;
+		draw();
 
-			for (int j = 0; j < attempt; j++) {
-				if (kicks[j] == secretWord[i]) {
-					find = 1;
-					break;
-				}
-			}
-
-			if (find) {
-				printf("%c ", secretWord[i]);
-			} else {
-				printf("_ ");
-			}
-		}
-		printf("\n");
-
-		kickCapture(kicks, &attempt);
+		kickCapture();
 
 	} while (!finish && !hanged);
 }
